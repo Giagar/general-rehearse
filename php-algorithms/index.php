9813e90fbcfa13ddaeaf6e4d72948fc2ebf88871
Gianpaolo <pao.barga@gmail.com>
2021-05-10 17:10:22 +0200

    done first five basic algorithms both in php and in js

--- php-algorithms/index.php
+++ php-algorithms/index.php
@@ -106,12 +106,62 @@
         function findLongestWordLength(str) {
             let longest = 0;
             str
-            .split(" ")
-            .forEach(el => el.length > longest ? longest = el.length : null);
+                .split(" ")
+                .forEach(el => el.length > longest ? longest = el.length : null);
             return longest;
         }
 
         document.querySelector(".js-result-algo4").innerHTML = findLongestWordLength("The quick brown fox jumped over the lazy dog");
+    </script>
+    <hr>
+
+    <h3>Algo5: Return Largest Numbers in Arrays</h3>
+    <?php
+    function largestOfFour($arr)
+    {
+        $result = [];
+        foreach ($arr as $list) {
+            $largest = $list[0];
+            foreach ($list as $num) {
+                $num > $largest ? $largest = $num : null;
+            }
+            $result[] = $largest;
+        }
+        return $result;
+    }
+
+    $result_a5 = largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+    // $print_a5 = "[ ";
+    // foreach ($result_a5 as $key_a5 => $num_a5) {
+    //     $key_a5 < count($result_a5) - 1 ? $print_a5 .= $num_a5 . ", " : $print_a5 .= $num_a5 . " ]";
+    // }
+    $result_a5 = implode(", ", $result_a5);
+    $print_a5 = "[ $result_a5 ]";
+    echo $print_a5;
+    ?>
+    <div class="js-result-algo5"></div>
+    <script>
+        function largestOfFour(arr) {
+            let result = [];
+            let largest;
+            arr.forEach(list => {
+                largest = list[0];
+                list.forEach(num => num > largest ? largest = num : null);
+                result.push(largest);
+            })
+            return result;
+        }
+
+        let result_a5 = largestOfFour([
+            [4, 5, 1, 3],
+            [13, 27, 18, 26],
+            [32, 35, 37, 39],
+            [1000, 1001, 857, 1]
+        ]);
+        
+        let print_a5 = `[ ${result_a5.join(", ")} ]`;
+
+        document.querySelector(".js-result-algo5").innerHTML = print_a5;
     </script>
     <hr>
 
