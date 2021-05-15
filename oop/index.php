7e29422797fdbfdb6f5be63fec28e171583d0866
Gianpaolo <pao.barga@gmail.com>
2021-05-15 21:03:30 +0200

    rehearsed visibility, inheritance, polymorphism, construct, exceptions

--- oop/index.php
+++ oop/index.php
@@ -12,6 +12,7 @@
 class Person {
     protected $name; // it can be accessed only from inside this class and its children classes
     private $surname = "Person_surname"; // it can be accessed only from inside this class
+    public $gender = "Person_gender";
 
     public function greetings() { // it can be accessed from everywhere
         echo "Hey!";
@@ -34,6 +35,10 @@ class CustomPerson extends Person {
 $person1 = new CustomPerson();
 $person1->setName("person1_name");
 echo $person1->getName();
+echo "<br>";
+echo $person1->getSurname();
+echo "<br>";
+echo $person1->gender;
 ?>
 
 <h2>Polymorphism</h2>
@@ -82,6 +87,71 @@ try{
     echo sayName(14);
 } catch (Exception $e) {
     echo "Eccezione: " . $e->getMessage();
+}
+echo "<br>";
+echo "dopo funzione";
+?>
+
+<hr>
+
+<h2>Rehearse</h2>
+<h3>Visibility, Inheritance, Polymorphism, Constructor</h3>
+<?php 
+class Animal {
+    protected $name;
+    private $id;
+    private $type;
+    
+    function __construct($id, $type) {
+        $this->id = $id;
+        $this->type = $type;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function voice() {
+        return "original voice";
+    }
+}
+
+class CustomAnimal extends Animal {
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($newName) {
+        $this->name = $newName;
+    }
+
+    public function voice() {
+        return "new voice";
+    }
+}
+
+$animal1 = new CustomAnimal(14, "type1");
+$animal1->setName("animal1_name");
+echo $animal1->getName();
+echo "<br>";
+echo $animal1->getId();
+echo "<br>";
+echo $animal1->voice();
+?>
+
+<h3>Exceptions</h3>
+<?php 
+function sum($a, $b) {
+    if(!is_int($a) || !is_int($b)) {
+        throw new Exception("Both of the args need to be a number");
+    }
+    return $a + $b;
+}
+
+try{
+    echo sum(9, 10);
+} catch(Exception $e) {
+    echo $e->getMessage();
 }
 ?>
 </body>
