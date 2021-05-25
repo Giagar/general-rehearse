d3030afee72a9c4ebf29c384c68f184967775276
Gianpaolo <pao.barga@gmail.com>
2021-05-25 11:53:09 +0200

    stripe: esempio scaricato funzionante

---
+++ practice/stripe-example/stripe-payments-sample-code/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '05704a6c0e98d1bddb52f098cc2796c91c48044b',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '05704a6c0e98d1bddb52f098cc2796c91c48044b',
+    ),
+    'stripe/stripe-php' => 
+    array (
+      'pretty_version' => 'v7.78.0',
+      'version' => '7.78.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6ec33fa8e9de2322be93d28dfd685661c67ca549',
+    ),
+  ),
+);
